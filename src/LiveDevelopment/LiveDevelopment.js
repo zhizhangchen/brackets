6579a9211045c437247504b47a255b23072a7e88
John Chen <zhang.z.chen@intel.com>
2012-11-12 10:40:45 +0800

    Make url wrapper a property of LiveDevelopment so that it will not lost when open() is called again

--- src/LiveDevelopment/LiveDevelopment.js
+++ src/LiveDevelopment/LiveDevelopment.js
@@ -116,6 +116,10 @@ define(function LiveDevelopment(require, exports, module) {
     var _liveDocument; // the document open for live editing.
     var _relatedDocuments; // CSS and JS documents that are used by the live HTML document
 
+    var _urlWrapper;
+    function setUrlWrapper(wrapper) {
+        _urlWrapper = wrapper;
+    }
     function _isHtmlFileExt(ext) {
         return (FileUtils.isStaticHtmlFileExt(ext) ||
                 (ProjectManager.getBaseUrl() && FileUtils.isServerHtmlFileExt(ext)));
@@ -464,11 +468,11 @@ define(function LiveDevelopment(require, exports, module) {
     }
 
     /** Open the Connection and go live */
-    function open(urlWrapper) {
+    function open() {
         var result = new $.Deferred(),
             promise = result.promise();
         var doc = _getCurrentDocument();
-        var argUrl = urlWrapper ? urlWrapper(doc.root.url) : doc.root.url;
+        var argUrl = _urlWrapper ? _urlWrapper(doc.root.url) : doc.root.url;
         var url = argUrl.split(" ");
         var browserStarted = false;
         var retryCount = 0;
@@ -713,5 +717,6 @@ define(function LiveDevelopment(require, exports, module) {
     exports.showHighlight       = showHighlight;
     exports.hideHighlight       = hideHighlight;
     exports.redrawHighlight     = redrawHighlight;
+    exports.setUrlWrapper       = setUrlWrapper;
     exports.init                = init;
 });
