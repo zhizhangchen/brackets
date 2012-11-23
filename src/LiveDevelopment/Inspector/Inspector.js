b9c04909e794306b19f3617ea56db402eed0f3f5
John Chen <zhang.z.chen@intel.com>
2012-11-23 17:48:10 +0800

    Hide the title in debugging mode

--- src/LiveDevelopment/Inspector/Inspector.js
+++ src/LiveDevelopment/Inspector/Inspector.js
@@ -202,7 +202,7 @@ define(function Inspector(require, exports, module) {
 
     function _toggleDevTools(show) {
         if (show) {
-            _editingElements = $('.content > :not(#main-toolbar), #sidebar, #sidebar-resizer, #status-bar, .nav').filter(':visible');
+            _editingElements = $('.content > :not(#main-toolbar), #sidebar, #sidebar-resizer, #status-bar, .nav, .title-wrapper').filter(':visible');
         }
         _editingElements.toggle(!show);
         $('#dev-tools').toggle(show);
