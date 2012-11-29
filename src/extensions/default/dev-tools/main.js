676e1307508f266ca1658790b29c1fff8e7fe161
John Chen <zhang.z.chen@intel.com>
2012-11-29 16:30:32 +0800

    Cleanup dev-tools code and use loadLessFile

--- src/extensions/default/dev-tools/main.js
+++ src/extensions/default/dev-tools/main.js
@@ -1,17 +1,17 @@
 /*
  * The MIT License (MIT)
- * Copyright (c) 2012 Dennis Kehrig. All rights reserved.
- *
+ * Copyright (c) 2012 Intel Corporation. All rights reserved.
+ * 
  * Permission is hereby granted, free of charge, to any person obtaining a
  * copy of this software and associated documentation files (the "Software"),
  * to deal in the Software without restriction, including without limitation
  * the rights to use, copy, modify, merge, publish, distribute, sublicense,
  * and/or sell copies of the Software, and to permit persons to whom the
  * Software is furnished to do so, subject to the following conditions:
- *
+ * 
  * The above copyright notice and this permission notice shall be included in
  * all copies or substantial portions of the Software.
- *
+ * 
  * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
  * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
  * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
@@ -19,12 +19,10 @@
  * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING
  * FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER
  * DEALINGS IN THE SOFTWARE.
- *
  */
 
-/*jslint vars: true, plusplus: true, devel: true, nomen: true, indent: 4, forin: true, maxerr: 50, regexp: true */
-/*global define, brackets, $, less, io */
-
+/*jslint vars: true, plusplus: true, devel: true, nomen: true, indent: 4, maxerr: 50, regexp: true, bitwise: true */
+/*global define, brackets, $, window */
 
 /**
  *
@@ -32,141 +30,88 @@
 define(function main(require, exports, module) {
     'use strict';
 
-    // --- Required modules ---
 
-    // Document Manager
     var CommandManager = brackets.getModule("command/CommandManager"),
         Commands       = brackets.getModule("command/Commands"),
         FileUtils      = brackets.getModule("file/FileUtils"),
         LiveDevelopment = brackets.getModule("LiveDevelopment/LiveDevelopment"),
-        Inspector      = brackets.getModule("LiveDevelopment/Inspector/Inspector");
+        ExtensionUtils = brackets.getModule("utils/ExtensionUtils"),
+        Inspector      = brackets.getModule
+                            ("LiveDevelopment/Inspector/Inspector");
 
-    // <style> tag containing CSS code compiled from LESS
-    var _$styleTag;
-    // The toolbar button
-    var _$button;
-    /** Loads a less file as CSS into the document */
-    var _$debugging = false;
-    function _loadLessFile(file, dir) {
-        var result = $.Deferred();
-
-        // Load the Less code
-        $.get(dir + file, function (code) {
-            // Parse it
-            var parser = new less.Parser({ filename: file, paths: [dir] });
-            parser.parse(code, function onParse(err, tree) {
-                console.assert(!err, err);
-                // Convert it to CSS and append that to the document head
-                $("<style>").text(tree.toCSS()).appendTo(window.document.head);
-                result.resolve();
-            });
-        });
-
-        return result.promise();
-    }
-
+    var _styleTag;
+    var _button;
+    var _debugging = false;
     function _onFrameNavigated(event, res) {
-        var elementsPanel;
-        if (Inspector.config.reuseDevToolsSocket) {
-            var inspector = $('iframe').contents()[0].defaultView.WebInspector;
-            if (inspector && inspector.panels &&
-                (elementsPanel = inspector.panels.elements)) {
-                elementsPanel.performSearch(res.frame.url);
-                setTimeout(function () {
-                    inspector.domAgent.searchResult(0, function (node) {
-                        var treeOutline = elementsPanel.treeOutline;
-                        var selectingNode = node && node.children[0];
-                        if (!selectingNode) return;
-                        elementsPanel.searchCanceled();
-                        elementsPanel.selectDOMNode(selectingNode, true);
-                        selectingNode.getChildNodes(function (children) {
-                            if (children.length) {
-                                elementsPanel.selectDOMNode(children[0], true);
-                                children[0].getChildNodes(function (grandChildren){
-                                    var selectingChild = children[0].ownerDocument.body || children[0].ownerDocument.documentElement;
-                                    if (selectingChild) {
-                                        elementsPanel.selectDOMNode(selectingChild, true);
-                                    }
-                                });
+        var elementsPanel,
+            inspector,
+            iframe = $('iframe');
+        if (iframe.length === 0)
+            return;
+        inspector = iframe.contents()[0].defaultView.WebInspector;
+        if (inspector && inspector.panels &&
+            (elementsPanel = inspector.panels.elements)) {
+            elementsPanel.performSearch(res.frame.url);
+            setTimeout(function () {
+                inspector.domAgent.searchResult(0, function (node) {
+                    var treeOutline = elementsPanel.treeOutline;
+                    var selectingNode = node && node.children[0];
+                    if (!selectingNode) return;
+                    elementsPanel.searchCanceled();
+                    elementsPanel.selectDOMNode(selectingNode, true);
+                    selectingNode.getChildNodes(function (children) {
+                        if (children.length === 0)
+                            return;
+                        elementsPanel.selectDOMNode(children[0], true);
+                        children[0].getChildNodes(function (grandChildren){
+                            var selectingChild =
+                                children[0].ownerDocument.body ||
+                                children[0].ownerDocument.documentElement;
+                            if (selectingChild) {
+                                elementsPanel.selectDOMNode
+                                    (selectingChild, true);
                             }
-                        })
+                        });
                     })
-                }, 1000);
-            }
+                })
+            }, 1000);
         }
-    }
-    /** Find this extension's directory relative to the brackets root */
-    function _extensionUrl() {
-        var path = FileUtils.getNativeModuleDirectoryPath(module) + "/";
-        if (path[0] !== "/" && path[1] === ":")
-            //We are on Windows
-            return "file:///" + path.replace(/\\/g, "/")
-        else
-            return "file://" + path;
-    }
-
-    /** Handles clicks of the V8 toolbar button */
-    function _onButtonClicked() {
-        if (Inspector.connected()) {
-            LiveDevelopment.close();
-            _$debugging = false;
-        }
-        else {
-            _$debugging = true;
-            CommandManager.execute(Commands.FILE_LIVE_FILE_PREVIEW, true);
-        }
-    }
-    // --- Loaders and Unloaders ---
-
-    function _loadStyle() {
-        return _loadLessFile("main.less", _extensionUrl()).done(function ($node) {
-            _$styleTag = $node;
-        });
-    }
-
-    function _unloadStyle() {
-        _$styleTag.remove();
-    }
-
-    /** Setup the V8 toolbar button, called by init */
-    function _loadButton() {
-        var result = new $.Deferred();
-
-        _loadStyle().done(function () {
-            _$button = $("<a>").text("Debug").attr({ href: "#", id: "denniskehrig-v8live-button" });
-            _$button.click(_onButtonClicked);
-            _$button.insertBefore('#main-toolbar .buttons #toolbar-go-live');
-            result.resolve();
-        }).fail(result.reject);
-
-        return result.promise();
     }
 
     function load() {
-        _loadStyle().done(function () {
-            _loadButton();
+        ExtensionUtils.loadLessFile(module, "main.less").done( function (node) {
+            _styleTag = node;
+            _button = $("<a>").text("Debug")
+                .attr({ href: "#", id: "denniskehrig-v8live-button" })
+                .insertBefore('#main-toolbar .buttons #toolbar-go-live')
+                .click(function () {
+                    if (Inspector.connected()) {
+                        LiveDevelopment.close();
+                        _debugging = false;
+                    }
+                    else {
+                        _debugging = true;
+                        CommandManager.execute
+                            (Commands.FILE_LIVE_FILE_PREVIEW, true);
+                    }
+                })
         });
         $(Inspector.Page).on("frameNavigated.dev-tools", _onFrameNavigated)
         $(Inspector).on("connect", function () {
-            _$button.toggleClass("bridged", _$debugging);
+            _button.toggleClass("bridged", _debugging);
         });
         $(Inspector).on("disconnect", function () {
-            _$debugging = false;
-            _$button.toggleClass("bridged", _$debugging);
+            _debugging = false;
+            _button.toggleClass("bridged", _debugging);
         });
     }
 
     function unload() {
+        _styleTag.remove();
     }
-
-
-    // --- Exports ---
 
     exports.load = load;
     exports.unload = unload;
-
-
-    // --- Initializiation ---
 
     load();
 });
