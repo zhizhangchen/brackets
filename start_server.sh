3815867eb509453ee6bfae13ee99a0e61eb5dd2a
John Chen <zhang.z.chen@intel.com>
2013-02-07 10:50:46 +0800

    Start server with geometry

--- start_server.sh
+++ start_server.sh
@@ -3,7 +3,7 @@ NOVNC_DIR=src/thirdparty/noVNC
 sudo apt-get install tightvncserver
 mkdir -p ~/.vnc
 cp $NOVNC_DIR/xstartup $NOVNC_DIR/start_emulator.sh ~/.vnc
-vncserver
+vncserver -geometry 520x700
 $NOVNC_DIR/utils/launch.sh --vnc localhost:5901 &
 
 node src/extensions/default/devices/EmulatorServer.js
